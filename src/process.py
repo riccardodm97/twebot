39917dafe28e23906d5ead245dfec90b6e085ea6
Riccardo De Matteo <51266633+riccardodm97@users.noreply.github.com>
2022-10-01 19:12:47 +0200

    v4 init

--- src/process.py
+++ src/process.py
@@ -1,5 +1,8 @@
 
 import re
+import collections
+import math 
+from datetime import datetime
 from string import punctuation
 from statistics import mean
 
@@ -13,6 +16,7 @@ from nltk.tokenize import TweetTokenizer
 from nltk.corpus import stopwords
 from nltk.corpus import stopwords
 from scipy.stats import zscore 
+import Levenshtein
 
 import src.globals as glob 
 
@@ -354,3 +358,124 @@ def process_dataset_v3(dataframe : pd.DataFrame, tw_for_features : int, tw_for_t
     return df 
 
 
+def process_dataset_v3(dataframe : pd.DataFrame, save_path) :
+    to_drop = ['neighbor','domain','profile.id','profile.id_str','profile.profile_location','profile.entities','profile.utc_offset','profile.time_zone','profile.lang',
+'profile.contributors_enabled','profile.is_translator','profile.is_translation_enabled','profile.profile_background_color','profile.profile_background_image_url','profile.profile_background_image_url_https',
+'profile.profile_background_tile','profile.profile_image_url','profile.profile_image_url_https','profile.profile_link_color','profile.profile_sidebar_border_color','profile.profile_sidebar_fill_color',
+'profile.profile_text_color','profile.has_extended_profile','neighbor.following', 'neighbor.follower']
+
+    rename_dict = {'profile.name': 'name', 'profile.screen_name': 'screen_name', 'profile.location': 'location','profile.description':'description',
+'profile.url' : 'url', 'profile.protected' : 'protected', 'profile.followers_count':'followers_count', 'profile.friends_count':'friends_count', 
+'profile.listed_count':'listed_count','profile.created_at' :'created_at', 'profile.favourites_count' :'favourites_count','profile.geo_enabled':'geo_enabled',
+'profile.verified':'verified', 'profile.statuses_count':'statuses_count','profile.profile_use_background_image' : 'background_image',
+'profile.default_profile' : 'default_profile', 'profile.default_profile_image':'default_profile_image'}
+
+    
+    def description_len(desc:str):
+        if desc == "":
+            return 0
+        else : 
+            return len(desc)
+
+    def fofo_ratio(f1: str ,f2: str):
+        f1,f2 = int(f1), int(f2)
+        if f2 == 0.0 : 
+            return f1
+        else : 
+            return f1/f2 
+
+    def numbers_in_str(s : str):
+        return len(re.findall(r'\d+', s))
+            
+    def hashtag_in_str(s: str):
+        return len(re.findall(r"""(?:\#+[\w_]+[\w\'_\-]*[\w_]+)""",s))
+
+    def urls_in_str(s: str):
+        urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', s)
+        return len(urls)
+
+    def bot_world_in_str(s: str):
+        return int(bool(re.search('bot', s, flags=re.IGNORECASE)))
+
+
+    def get_str_entropy(s : str):
+        counter = collections.Counter(s)
+        screen_name_entropy = 0
+        for key, cnt in counter.items():
+            prob = float(cnt) / len(s)
+            screen_name_entropy += -1 * prob * math.log(prob, 2)
+        return screen_name_entropy
+        
+    def get_account_age(created_at : str) :
+        Twibot_20_FORMAT = f'%a %b %d %H:%M:%S %z %Y'
+        creation_date = datetime.strptime(created_at,Twibot_20_FORMAT).date()
+        present_date = datetime.now().date()
+        user_age = (present_date - creation_date).days
+        return int(user_age)
+
+
+    def get_avg_tweets_per_day(created_at : str, tot_tweets : str):
+        age = get_account_age(created_at)
+        avg_tweets = float(int(tot_tweets) / age)
+        return avg_tweets
+
+    def lev_name_screenName(name : str, screen_name : str) :
+        return Levenshtein.distance(name,screen_name)
+
+    
+
+    df = dataframe.copy(deep=True)
+
+    df = df.drop(to_drop,axis=1).reset_index(drop=True)  # drop unuseful columns 
+    df.rename(columns=rename_dict, inplace=True)         # rename all columns 
+    df = df.applymap(lambda x : x.rstrip())              # strip the trailing space added by twitter 
+
+
+    feature_df = df[['account_id','label','split']].reset_index(drop=True)
+    feature_df['label'] = feature_df['label'].astype(int)
+
+
+    #KOUVELA 
+    feature_df['has_desc'] = (df['description'] != '').astype(int)  #check lenght 1486
+    feature_df['has_location'] = (df['location'] != '').astype(int)  #check lenght 3386
+    feature_df['has_url'] = (df['url'] != 'None').astype(int)  #check lenght 5965
+    feature_df['is_verified'] = (df['verified'] == 'True').astype(int)  #check lenght 2962
+    feature_df['bot_word_in_name'] = df['name'].apply(bot_world_in_str)
+    feature_df['bot_word_in_screen_name'] = df['screen_name'].apply(bot_world_in_str)
+    feature_df['bot_word_in_description'] = df['description'].apply(bot_world_in_str)
+    feature_df['name_len'] = df['name'].apply(len)
+    feature_df['screen_name_len'] = df['screen_name'].apply(len)
+    feature_df['description_len'] = df['description'].apply(description_len)
+    feature_df['followings_count'] = df['friends_count'].astype(int)
+    feature_df['followers_count'] = df['followers_count'].astype(int)
+    feature_df['fofo_ratio'] = df.apply(lambda x: fofo_ratio(x['followers_count'], x['friends_count']), axis=1)
+    feature_df["tweets_count"] = df['statuses_count'].astype(int)
+    feature_df["listed_count"] = df['listed_count'].astype(int)
+    feature_df["num_in_name"] = df['name'].apply(numbers_in_str)
+    feature_df["num_in_screen_name"] = df['screen_name'].apply(numbers_in_str)
+    feature_df['hashtag_in_description'] = df['description'].apply(hashtag_in_str)
+    feature_df['urls_in_description'] = df['description'].apply(urls_in_str)
+    feature_df['def_image'] = (df['default_profile_image'] == 'True').astype(int)
+    feature_df['def_profile'] = (df['default_profile'] == 'True').astype(int)
+
+    #KANTEPE 
+    feature_df['is_protected'] = (df['protected'] == 'True').astype(int)
+    feature_df['screen_name_entropy'] = df['screen_name'].apply(get_str_entropy)
+    feature_df['tweet_freq'] = df.apply(lambda x: get_avg_tweets_per_day(x['created_at'], x['statuses_count']), axis=1)
+
+    #KNAUTH
+    feature_df['is_geo_enabled'] = (df['geo_enabled'] == 'True').astype(int)
+    feature_df["favourites_count"] = df['favourites_count'].astype(int)
+    feature_df['use_background_img'] = (df['background_image'] == 'True').astype(int)
+    feature_df['lev_dist_name_screeName'] = df.apply(lambda x: lev_name_screenName(x['name'], x['screen_name']), axis=1)
+
+
+
+
+
+
+
+
+
+
+    
